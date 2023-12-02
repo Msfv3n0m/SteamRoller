c3e426c60127e76e89a252baad096741253a476f
7ermina1 <92611061+7ermina1@users.noreply.github.com>
2023-12-01 19:00:15 -0500

    Update driver.ps1
    changed the $passFuncs ChangeLocalPasswords function to a working command

--- driver.ps1
+++ driver.ps1
@@ -144,7 +144,7 @@ function ChangeLocalPasswords ($ServersList, $cd, $admin) {
   Write-Host "Changing local passwords" -ForegroundColor Green
   $newPass="Superchiapet1"
   $cmdCommand1 = @"
-  for /f "skip=1" %a in ('net user') do net user %a $newPass 
+  For /f %a in ('wmic useraccount get name') do net user %a password123?
 "@ # > null
   $ServersList | %{
     Write-Host "Attempting to change passwords on $_" -ForegroundColor Green 
